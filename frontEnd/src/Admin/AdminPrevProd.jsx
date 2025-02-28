406367999f63aec5dd0b9ebde8ec843eef4e2dc8
Scottiieee <splendidhoodies@gmail.com>
2025-03-01 01:21:01 +0800

    Fix ui structure

--- frontEnd/src/Admin/AdminPrevProd.jsx
+++ frontEnd/src/Admin/AdminPrevProd.jsx
@@ -1,36 +1,44 @@
-export const AdminPrevProd = () => {
-  return (
-    <div className="  flex flex-wrap justify-center gap-[1rem] p-[0.5rem]    border">
-      {/* div for product */}
-      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem] flex flex-col justify-around items-center">
-        Product 1
-        <div className="flex flex-col  gap-[1rem]">
-          <button className="border text-black p-[0.5rem] cursor-pointer">
-            Edit
-          </button>
-          <button className="border text-black p-[0.5rem]  cursor-pointer">
-            Remove
-          </button>
-          <button className="border text-black p-[0.5rem] cursor-pointer">
-            Publish
-          </button>
-        </div>
-      </div>
+import HoodieImg from "../Images/HoodieProd.png";
 
-      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem] flex flex-col justify-around items-center">
-        Product 2
-        <div className="flex flex-col  gap-[1rem]">
-          <button className="border text-black p-[0.5rem] cursor-pointer">
-            Edit
-          </button>
-          <button className="border text-black p-[0.5rem]  cursor-pointer">
-            Remove
-          </button>
-          <button className="border text-black p-[0.5rem] cursor-pointer">
-            Publish
-          </button>
+export const AdminPrevProd = () => {
+  // Sample Product Data
+  const products = [
+    { id: "02000319", name: "Oversized Hoodie", stock: 134, price: 850 },
+    { id: "02000320", name: "Zip-Up Hoodie", stock: 98, price: 900 },
+    { id: "02000321", name: "Classic Hoodie", stock: 76, price: 950 },
+  ];
+
+  return (
+    <div className="p-4 flex flex-wrap justify-center gap-4">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-white rounded-lg shadow-md p-6 flex flex-col items-center gap-4 transition-all duration-300 w-full max-w-[18rem] md:max-w-[22rem]"
+        >
+          {/* Product Image */}
+          <img
+            src={HoodieImg}
+            alt={product.name}
+            className="w-[12rem] h-[12rem] border rounded-lg object-cover"
+          />
+
+          {/* Product Name */}
+          <p className="font-semibold text-lg text-center">{product.name}</p>
+
+          {/* Action Buttons */}
+          <div className="flex flex-col gap-2 w-full">
+            <button className="w-full border py-2 text-black hover:bg-gray-100 transition">
+              Edit
+            </button>
+            <button className="w-full border py-2 text-black hover:bg-red-100 transition">
+              Remove
+            </button>
+            <button className="w-full border py-2 text-black hover:bg-green-100 transition">
+              Publish
+            </button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
