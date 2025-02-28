999c17e8b5eb17273b4dccea8e24dda45a3b0fce
Scottiieee <splendidhoodies@gmail.com>
2025-03-01 00:20:02 +0800

    add Ui structure for  prod preview

--- frontEnd/src/Admin/AdminPrevProd.jsx
+++ frontEnd/src/Admin/AdminPrevProd.jsx
@@ -1,11 +1,35 @@
 export const AdminPrevProd = () => {
   return (
     <div className="  flex flex-wrap justify-center gap-[1rem] p-[0.5rem]    border">
-      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem]">
+      {/* div for product */}
+      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem] flex flex-col justify-around items-center">
         Product 1
+        <div className="flex flex-col  gap-[1rem]">
+          <button className="border text-black p-[0.5rem] cursor-pointer">
+            Edit
+          </button>
+          <button className="border text-black p-[0.5rem]  cursor-pointer">
+            Remove
+          </button>
+          <button className="border text-black p-[0.5rem] cursor-pointer">
+            Publish
+          </button>
+        </div>
       </div>
-      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem]">
-        Product 1
+
+      <div className=" bg-white p-[1rem] h-[30rem]  max-w-[30rem] min-w-[18rem] flex flex-col justify-around items-center">
+        Product 2
+        <div className="flex flex-col  gap-[1rem]">
+          <button className="border text-black p-[0.5rem] cursor-pointer">
+            Edit
+          </button>
+          <button className="border text-black p-[0.5rem]  cursor-pointer">
+            Remove
+          </button>
+          <button className="border text-black p-[0.5rem] cursor-pointer">
+            Publish
+          </button>
+        </div>
       </div>
     </div>
   );
